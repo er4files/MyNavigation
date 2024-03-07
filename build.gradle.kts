7c265a68c2fbf6e4234650125001b0cacfecda2a
er4files <rahmaderasugiarto@gmail.com>
2024-03-07 16:50:16 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript {
+    dependencies {
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
+    }
+}
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
